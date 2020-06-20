6478a63047dd989e98da3633abbd939a602e398b
Kingsr <i@kingsr.cc>
2020-06-20 14:12:43 +0800

    init

---
+++ js/chunk-2d2252b5.2d707356.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d2252b5"],{e2f8:function(n,e,t){"use strict";t.r(e);var u=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div")},c=[],l=t("2877"),r={},s=Object(l["a"])(r,u,c,!1,null,null,null);e["default"]=s.exports}}]);
+//# sourceMappingURL=chunk-2d2252b5.2d707356.js.map
